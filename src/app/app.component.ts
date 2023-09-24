d01061c3030336f498ccbdf692b99a5fa92f914a
Anna <annamotsikula@gmail.com>
2023-09-24 23:11:17 +0400

    one way data binding, decorator: component & module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Student } from './app.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // OR: 
+  // template: `<h2>This is second header</h2>`
+  // styles: ['h2 { color: red }']
 })
 export class AppComponent {
-  title = 'ng-test-app';
+  title = 'ng-test-app-123-123';
+
+  decimalNumber : number = 20;
+  isWinter: boolean = false;
+
+  ids: number[] = [2,3,4,5,6,7]
+
+  student: Student = {
+    age: 21,
+    name: 'Kate',
+    isStudent: true,
+    gpa: 3.12
+
+  }
+
+
 }
---
+++ src/app/app.interface.ts
@@ -0,0 +1,7 @@
+export interface Student {
+    name: string,
+    age: number,
+    isStudent: boolean,
+    faculty?: string,
+    gpa: number
+}
